b1d4016f6273a080f2d9b698a5ecb4dea143275a
PimpatDev <pimpatdev@gmail.com>
2019-11-17 22:17:48 +0700

    aa5
    aa5

---
+++ Exercise_5_1_Peerapat_P.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter your number 1 : "))
+num2 = int(input("Enter your number 2 : "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = int(num1 * num2)
+division = int(num1 / num2)
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
---
+++ Exercise_5_2_Peerapat_P.py
@@ -0,0 +1,4 @@
+s = int(input("Enter distance (km) : "))
+t = int(input("Enter time (minute): "))
+velocity = int(s/t)
+print(velocity, "km/h")
