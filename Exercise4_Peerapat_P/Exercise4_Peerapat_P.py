e28b4b911cf12a451003d408d04521a42305dc5e
PimpatDev <pimpatdev@gmail.com>
2019-11-17 22:13:42 +0700

    Revert "Add project"
    This reverts commit bbe73360963cd8a3123cf2fa4cd2749bfebfd019.

--- Exercise4_Peerapat_P/Exercise4_Peerapat_P.py
+++
@@ -1,9 +0,0 @@
-english = 80
-business = 80
-comSystem = 85
-programming = 85
-print("--- Your Score ---")
-print("Foundation English : ", english)
-print("General Business : ", business)
-print("Introduction to Computer Systems : ", comSystem)
-print("Computer Programming : ", programming)
